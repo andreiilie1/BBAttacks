7d0ecefa2253ed310b1455885c6c4141dd1aeb78
Andrei Ilie <andrei.ilie@bolt.eu>
2021-06-30 00:55:11 +0300

    utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,30 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
 def cap(value, inf, sup):
     if value <= inf:
         return inf
     if value >= sup:
         return sup
     return value
+
+
+def plot_digits(X, Y):
+    for i in range(20):
+        plt.subplot(5, 4, i+1)
+        plt.tight_layout()
+        plt.imshow(X[i].reshape(28, 28), cmap='gray')
+        plt.title('Digit:{}'.format(Y[i]))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+    
+    
+def top_k_accuracy(y_true, y_pred, k=1):
+    '''From: https://github.com/chainer/chainer/issues/606
+    
+    Expects both y_true and y_pred to be one-hot encoded.
+    '''
+    argsorted_y = np.argsort(y_pred)[:,-k:]
+    return np.any(argsorted_y.T == y_true.argmax(axis=1), axis=0).mean()
